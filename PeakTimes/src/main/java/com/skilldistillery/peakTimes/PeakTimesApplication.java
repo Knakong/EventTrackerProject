6b2ea6eec4d0d45aa63316a7731b85230a0a0546
Knakong <roathana.kong@gmail.com>
2023-01-20 16:31:46 -0700

    set up Spring Project

---
+++ PeakTimes/src/main/java/com/skilldistillery/peakTimes/PeakTimesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.peakTimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakTimesApplication.class, args);
+	}
+
+}
